b58497a394bc07c9ce5e2bee3b93ba6e0fec3090
Michael Hurhangee <m.hurhangee@me.com>
2025-06-25 20:10:53 +0000

    remove mastra (conflicts with the aisdk beta)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  serverExternalPackages: ['@mastra/*'],
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
